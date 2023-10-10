e7eda17e946bb88122e01439904b99d81153d8c9
Your Name <you@example.com>
2023-10-10 11:11:32 +0530

    evenodd

---
+++ even or odd .py
@@ -0,0 +1,5 @@
+n=int(input("enter the value:")) #input=even or odd
+if n%2==0:
+    print("it is an even number") #if is even numbers
+else:                             #else is odd numbers
+    print("it is an odd number")
